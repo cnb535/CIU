8650ba23f7a9c78a51d06d2469813c905ccd4dbc
Christina Bertram <Jamerican92grl@MacBook-Pro.home>
2018-05-04 20:41:39 -0400

    React setup

---
+++ client/src/index.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import registerServiceWorker from "./registerServiceWorker";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+// registerServiceWorker();
+// 
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "material-ui/Button";
+import App from "./App";
+
+// function App() {
+//   return (
+//     <Button variant="raised" color="primary">
+//       Hello World
+//     </Button>
+//   );
+// }
+
+ReactDOM.render(<App />, document.querySelector("#app"));
+
